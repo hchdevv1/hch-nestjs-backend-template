5e0d9358d3b9c45c98ca1512f2c88f15e03c0139
dakinuser1 <dakin.user1@gmail.com>
2026-07-14 12:44:49 +0700

    feat(config): add application configuration

---
+++ src/config/configuration.ts
@@ -0,0 +1,8 @@
+export default () => ({
+  app: {
+    name: process.env.APP_NAME || 'HCH NestJS Backend Template',
+    env: process.env.APP_ENV || 'development',
+    host: process.env.APP_HOST || '0.0.0.0',
+    port: Number(process.env.APP_PORT || 3000),
+  },
+});
